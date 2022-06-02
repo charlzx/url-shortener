b7df94bf4bed7219a4a92dd062b6e5de36151ecb
Charles Obuzor <charlesobuzor@outlook.com>
2022-06-02 16:26:36 +0100

    generate short links

--- js/script.js
+++ js/script.js
@@ -1,3 +1,105 @@
-// document.body.innerHTML = `
+const form = document.querySelector('#form')
+const label = document.querySelector('#label')
+const input = document.querySelector('#form > div > input[type="text"]')
+const error = document.querySelector('#error')
+const shortLinks = document.querySelector('#short-links')
+const loader = document.querySelector('#loader')
 
-// `
+let inputLink;
+
+input.style.border = '3px solid transparent'
+
+
+class Link {
+   async sendInput (url){
+      let request = await fetch(`https://api.shrtco.de/v2/shorten?url=${url}`)
+
+      request.onload = () => {
+         let spinner = document.createElement('img')
+         spinner.src = "../images/loading.svg"
+
+         loader.appendChild(spinner)
+      }
+
+      let response = await request.json()
+
+      return response;
+   }
+}
+
+form.addEventListener('submit', (e) => {
+   inputLink = input.value
+
+   input.value = ""
+
+   let newLink = new Link
+
+   if (inputLink.length > 0) {
+
+      newLink.sendInput(inputLink)
+      .then(data => {
+         console.log(data)
+
+         if (data.ok === true){
+            shortLinks.innerHTML += `
+            <div class="shortLinks">
+               <p>${data.result.original_link}</p>
+               
+               <hr>
+               
+               <div>
+                  <a href="https://${data.result.short_link}" target="_blank">${data.result.short_link}</a>
+                  <button>Copy</button>
+               </div>
+            </div>
+            `
+         }
+
+         // display errors
+         if (data.ok === false && data.error_code === 2){
+            error.innerHTML = "This is not a valid URL"
+            
+            error.style.display = "block"
+         } else if (data.ok === false && data.error_code === 3){
+            error.innerHTML = "Rate limit reached. Wait a second and try again"
+            
+            error.style.display = "block"
+         } else if (data.ok === false && data.error_code === 6){
+            error.innerHTML = "Unknown Error."
+            
+            error.style.display = "block"
+         } else if (data.ok === false && data.error_code === 10){
+            error.innerHTML = "You are trying to shorten a <a href='https://shrtco.de/disallowed' target='_blank'>disallowed Link.</a>"
+            
+            error.style.display = "block"
+         } else if (data.ok === false) {
+            error.innerHTML = "Unknown Error."
+            
+            error.style.display = "block"
+         }
+
+         // display the error for only 3 secs
+         if (data.ok === false){
+            setTimeout(function(){
+               error.innerHTML = ""
+               error.style.display = "none"
+            }, 3000)
+         }
+
+      })
+      .catch(error => {
+         console.log(error)
+      })
+      
+   } else {
+      input.style.border = '3px solid red'
+      label.innerHTML = 'The Input field is empty'
+
+      setTimeout(function (){
+         input.style.border = '3px solid transparent'
+         label.innerHTML = ''
+      }, 3000)
+   }
+
+   e.preventDefault();
+})
